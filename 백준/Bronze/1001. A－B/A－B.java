8b475b17cbff224587c69680c32241155f7a6f46
Chosunin <132040493+Chosunin@users.noreply.github.com>
2025-08-19 20:14:05 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args){
+    Scanner user = new Scanner(System.in);
+    int a = user.nextInt(), b = user.nextInt();
+    
+    System.out.println(a-b);
+}
+    
+}
