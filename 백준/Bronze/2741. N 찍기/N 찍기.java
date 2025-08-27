e06f401828b239dd376052cbc3fc6776d97cce1e
Chosunin <132040493+Chosunin@users.noreply.github.com>
2025-08-27 22:21:58 +0900

    [Bronze V] Title: N 찍기, Time: 788 ms, Memory: 32776 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int i = 1, j = sc.nextInt();
+        while(i <= j){
+            System.out.println(i);
+            i++;
+        }
+
+
+    }
+    }
