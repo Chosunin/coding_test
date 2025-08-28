8ce88c5933b218ec9cc6af9749cf0be7f325a198
Chosunin <132040493+Chosunin@users.noreply.github.com>
2025-08-28 22:41:09 +0900

    [Bronze V] Title: A+B - 3, Time: 192 ms, Memory: 17928 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+
+
+    for(int tt = 0; tt < t; tt++){
+        int i = sc.nextInt(), j = sc.nextInt();
+        System.out.println(i + j);
+
+    }
+
+    sc.close();
+
+    }
+}
